e4ccad9d7ca54b432fb11dedc976124e1ca00694
THARUNKUMAR M <tharunkumarm.23cse@kongu.edu>
2025-04-23 16:07:43 +0530

    Solve Search a 2D Matrix

---
+++ leetcodes/Search_a_2D_Matrix.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0)
+            return false;
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int start = 0;
+        int end = rows * cols - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            int midElement = matrix[mid / cols][mid % cols];
+
+            if (midElement == target) {
+                return true;
+            } else if (midElement < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return false;
+    }
+}
