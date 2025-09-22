0f49b24d8d47f059485d473367eb48cef757bc90
THARUNKUMAR M <tharunkumarm.23cse@kongu.edu>
2025-09-22 10:28:07 +0530

    Solve Climbing Stairs

---
+++ LEETCODE/Climbing_Stairs.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    // int climbStairs(int n) {
+        // vector<int>memo(n+1,-1);
+        // return climbStairs(n,memo);
+    // }
+    // int climbStairs(int n,vector<int>&memo){
+    //     if(memo[n]!=-1) return memo[n];
+    //     if(n<=2) return memo[n]=n; // if(n<2) return memo[n]=1;
+    //     return memo[n]=climbStairs(n-1,memo)+climbStairs(n-2,memo);
+    // }
+    int climbStairs(int n){
+        vector<int> dp(n+1,0);
+        if(n<=2) return n;
+        dp[1] =1 ;
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        
+        }
+        return dp[n];
+    }
+};
