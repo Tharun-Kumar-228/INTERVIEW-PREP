009ed3f30f2008e201f371fe0fee269f234533ee
THARUNKUMAR M <tharunkumarm.23cse@kongu.edu>
2025-11-26 21:24:46 +0530

    Solve Remove Duplicates from Sorted List II

---
+++ leetcodes/Remove_Duplicates_from_Sorted_List_II.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (!head) return nullptr;
+
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+
+        ListNode* prev = dummy;
+        ListNode* curr = head;
+        while(curr){
+            bool duplicate = false;
+            while(curr->next && curr->val == curr->next->val){
+                duplicate = true;
+                curr = curr->next;
+            }
+
+            if (duplicate){
+                prev->next = curr->next;
+                curr= curr->next;
+            }else{
+            prev = curr;
+            curr = curr->next;
+            }
+        }
+        return dummy->next;
+    }
+};
