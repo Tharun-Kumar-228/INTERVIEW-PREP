c3e22126640888721034a78238aaba1786a19fbb
THARUNKUMAR M <tharunkumarm.23cse@kongu.edu>
2025-12-04 14:46:02 +0530

    Solve Path Sum

---
+++ LEETCODE/Path_Sum.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        int sum =0;
+        if(root == null) return false;
+        return finder(root,targetSum,sum);
+        
+    }
+    public boolean finder(TreeNode root, int targetSum,int sum){
+        if(root == null){
+            return false;
+        }
+        sum = sum+ root.val;
+        if(root.left == null && root.right == null)
+        return sum==targetSum;
+
+        return finder(root.right,targetSum,sum)||finder(root.left,targetSum,sum);
+    }
+}
