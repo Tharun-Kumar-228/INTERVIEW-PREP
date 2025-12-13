6040d60a973f65d11f60d98b10721963ed92e132
THARUNKUMAR M <tharunkumarm.23cse@kongu.edu>
2025-12-13 21:56:37 +0530

    Solve Path Sum

---
+++ leetcodes/Path_Sum.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int target) {
+        if(root ==NULL) return false;
+        else if(root->left == NULL && root->right == NULL){
+            return root->val == target;
+        }
+        else
+            return hasPathSum(root->left,target-root->val)||hasPathSum(root->right,target-root->val);
+    }
+};
