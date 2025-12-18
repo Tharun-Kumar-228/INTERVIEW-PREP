00021c6792c97d89a069fd893e15da36552c8fc2
THARUNKUMAR M <tharunkumarm.23cse@kongu.edu>
2025-12-18 10:17:25 +0530

    Solve Island Perimeter

---
+++ leetcodes/Island_Perimeter.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) {
+                    return dfs(grid, i, j);
+                }
+            }
+        }
+        return 0;
+    }
+
+    int dfs(vector<vector<int>>& grid, int i, int j) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+      
+        if (i < 0 || j < 0 || i >= n || j >= m)
+            return 1;
+
+      
+        if (grid[i][j] == 0)
+            return 1;
+
+      
+        if (grid[i][j] == -1)
+            return 0;
+
+      
+        grid[i][j] = -1;
+
+        return dfs(grid, i + 1, j)
+             + dfs(grid, i - 1, j)
+             + dfs(grid, i, j + 1)
+             + dfs(grid, i, j - 1);
+    }
+};
